0983f359fed19126d0d2682a5b365039420811f6
azsxza <az631132770@gmail.com>
2020-12-11 15:20:40 +0800

    添加项目文件。

---
+++ WindowsProject2/WindowsProject2.cpp
@@ -0,0 +1,13 @@
+﻿#include<Windows.h>
+
+HWND ghMainWnd = 0;
+bool InitWindowsApp(HINSTANCE instanceHandle, int show);
+
+int Run();
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WindMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, inr nCmdShow)
+{
+
+}
