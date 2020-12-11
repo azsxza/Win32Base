d354400a1f37f883891f78f004c5c0dbf2d8dfc4
azsxza <az631132770@gmail.com>
2020-12-11 16:58:16 +0800

    update complete window

--- WindowsProject2/WindowsProject2.cpp
+++ WindowsProject2/WindowsProject2.cpp
@@ -7,7 +7,79 @@ int Run();
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-int WINAPI WindMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, inr nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
 {
+	if (!InitWindowsApp(hInstance, nCmdShow))
+		return 0;
+	return Run();
+}
 
+bool InitWindowsApp(HINSTANCE instanceHandle, int show)
+{
+	WNDCLASS wc;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = instanceHandle;
+	wc.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(0, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = 0;
+	wc.lpszClassName = L"Basic wnd class";
+
+	if (!RegisterClass(&wc))
+	{
+		MessageBox(0, L"Register Class Failed", 0, 0);
+		return false;
+	}
+
+	ghMainWnd = CreateWindow(L"Basic Wnd Class", L"Win32 Basic", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, instanceHandle, 0);
+	if (ghMainWnd == 0)
+	{
+		MessageBox(0, L"Create Window Filed", 0, 0);
+		return false;
+	}
+	ShowWindow(ghMainWnd, show);
+	UpdateWindow(ghMainWnd);
+	return true;
+}
+
+int Run()
+{
+	MSG msg = { 0 };
+	BOOL bRet = 1;
+	while ((bRet = GetMessage(&msg, 0, 0, 0)) != 0)
+	{
+		if (bRet == -1)
+		{
+			MessageBox(0, L"Get Message Failed", L"Errot", MB_OK);
+			break;
+		}
+		else
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_LBUTTONDOWN:
+		MessageBox(0, L"Hello,World", L"Hello", MB_OK);
+		return 0;
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+			DestroyWindow(ghMainWnd);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
